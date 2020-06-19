a2d3e2926c2d8f2b27ba7463dc3a30645b7c2eee
Devin Duval <devinduval09@gmail.com>
2020-06-19 10:56:24 -0700

    "initial commit"

--- Refresher.py
+++ Refresher.py
@@ -1,2 +1,10 @@
 #Python Data types
-import tkinter
+import tkinter as tk
+from tkinter import ttk as ttk
+
+#create a popup to input integers
+intEntry = tk.tk()
+intLabel = ttk.Label(intEntry,text = "Enter an integer", font = ("Time Roman",12))
+intLabel.pack(side = left)
+intEnt = entry(intEntry, cursor = "dot")
+intEnt.pack(side = "right")
