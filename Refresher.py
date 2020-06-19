f397de6dc0a94b8f33c4762e1ffc9320a9bb8100
Devin Duval <devinduval09@gmail.com>
2020-06-19 11:06:24 -0700

    not working

--- Refresher.py
+++ Refresher.py
@@ -3,8 +3,8 @@ import tkinter as tk
 from tkinter import ttk as ttk
 
 #create a popup to input integers
-intEntry = tk.tk()
+intEntry = tk.Tk()
 intLabel = ttk.Label(intEntry,text = "Enter an integer", font = ("Time Roman",12))
-intLabel.pack(side = left)
-intEnt = entry(intEntry, cursor = "dot")
+intLabel.pack(side = "left")
+intEnt = ttk.Entry(intEntry, cursor = "dot")
 intEnt.pack(side = "right")
