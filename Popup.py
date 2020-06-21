f83b14589473454076e8f60742507e912b2daa42
Devin Duval <devinduval09@gmail.com>
2020-06-20 17:37:17 -0700

    Popup version alpha

--- Popup.py
+++ Popup.py
@@ -3,10 +3,11 @@ import tkinter as tk
 from tkinter import tkk as tkk
 
 #make a popup box with an input button, output label, and quit button
-#input: button function
+#input: button function, instructions
 
 class Popup():
 	def __init__(self,instructions,press):
+		x = 0
 		self = tk.Tk()
 		self.geometry("200x200")
 		#label with instructions
@@ -22,6 +23,14 @@ class Popup():
 		lblOutput = tkk.Button(self,text="")
 		lblOutput.pack()
 		#action button
-		Baction=tkk.Button(self,text="Do instructions",command=press)
+		Baction=tkk.Button(self,text="Do instructions",command=writeout)
 		Baction.pack()
+
+		#function to write output label
+		def writeout():
+			if len(Entry.get()) > 0:
+				x = press(Entry.get())
+				lblOutput.config(text=str(x))
+			else:
+				lblOutput.config(text="")
 
