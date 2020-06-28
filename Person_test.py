b775d1da11e8f96a24347b85499341c2c31b843b
Devin Duval <devinduval09@gmail.com>
2020-06-27 20:25:36 -0700

    changed person class to have contact info and assemble a resume

--- Person_test.py
+++ Person_test.py
@@ -1,10 +1,21 @@
 import Popup
 import clsPerson
+global per1
+per1 = clsPerson
+
+
+def createWorkHist(commaList):
+	global per1
+	data = commaList.split(",")
+	per1.addWorkExp(data[0],data[1],data[2],data[3])
+	return per1.createResume()
 
 def initPerson(commaList):
+	global per1
 	data = commaList.split(",")
 	per1 = clsPerson.clsPerson(data[0],data[1],data[2],data[3])
+	p2 = Popup.Popup("Enter Job Title,company,start date,end date:",createWorkHist,"word")
 
-	return "Name: {} \n Birthday: {} \n Profession: {}".format(per1.getFirstName() + " " + per1.getLastName(),per1.getBirthday(),per1.getProfession())
 
-p1 = Popup.Popup("Enter firstname,lastname,birthday,profession",initPerson,"word")
+
+p1 = Popup.Popup("Enter firstname,lastname,phone number,email",initPerson,"word")
--- clsPerson.py
+++ clsPerson.py
@@ -1,7 +1,8 @@
 class clsPerson:
 
-	def __init__(self,firstname,lastname,birthday,profession):
-		self.person = (firstname,lastname,birthday,profession)
+	def __init__(self,firstname,lastname,phoneNumber,email):
+		self.person = (firstname,lastname,phoneNumber,email)
+		self.workExp= ()
 
 	def getFirstName(self):
 		return self.person[0]
@@ -9,11 +10,18 @@ class clsPerson:
 	def getLastName(self):
 		return self.person[1]
 
-	def getBirthday(self):
+	def getPhoneNumber(self):
 		return self.person[2]
 
-	def getProfession(self):
+	def getEmail(self):
 		return self.person[3]
 
-	def addInformation(tuple):
-		self = self.person[0:len(person)] + tuple
+	def addWorkExp(self,jobtitle,company,sDate,eDate):
+		self.workExp = self.workExp + (jobtitle,company,sDate,eDate)
+
+	def createResume(self):
+		resume = "{}\n Phone: {}\n Email: {}\n".format(self.person[0] + self.person[1],self.person[2],self.person[3])
+		for x in range(0,len(self.workExp),4):
+			resume = resume + "\nTitle: {}\nCompany: {}\nFrom: {} to {}\n".format(self.workExp[x],self.workExp[x+1],self.workExp[x+2],self.workExp[x+3])
+		return resume
+
